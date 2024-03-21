dd48bd22e8336648ec78980508cecd67559df5e5
bbarkhouse-hashicorp <84409718+bbarkhouse-hashicorp@users.noreply.github.com>
2024-03-20 22:33:49 -0400

    container mount path

--- azure/boundary-worker.tf
+++ azure/boundary-worker.tf
@@ -18,14 +18,15 @@ resource "azurerm_container_group" "container" {
     }
     volume {
       name = "boundary-config"
-      mount_path = "/tmp/boundary-config"
+      mount_path = "/tmp"
       git_repo {
         url = "https://github.com/bfbarkhouse/hashistack-secure-infra-workflow"
+        directory = "/tmp/boundary-config"
       }
     }
     commands = [
-        "sudo mv /tmp/boundary-config/azure/boundary-worker.hcl /boundary/config.hcl",
-        "sudo rm -rf /tmp/boundary-config",
+        "mv /tmp/boundary-config/azure/boundary-worker.hcl /boundary/config.hcl",
+        "rm -rf /tmp/boundary-config",
         "boundary-enterprise"
         ] 
   }
